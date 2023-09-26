616ac4d1352ba676994ff3a914535ae5e00f712a
Nicolas MUNOZ <nmunoz@paul-f3Br2s2.clusters.42paris.fr>
2023-09-26 18:13:22 +0200

    Fixing mysql access

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -5,48 +5,18 @@ set -eux
 DIRECTORY_DATABASE=/var/lib/mysql/wordpress
 
 if [ ! -d "$DIRECTORY_DATABASE" ]; then
-		/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
-	until mysqladmin ping &>/dev/null; do
-		sleep 1
-	done
+	echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" | mysql -u root
 
-	#mysql command is a client for connecting and communicating with the
-	#the MYSQL database.
-	#-u root specifies that the client should connect as the root user,
-	#which has admin privileges
+	echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
 
-	#ALTER USER is used to modify the properties of an existing user
-	#in this case we are setting the password for the root user of the MYSQL instance
+	echo "CREATE USER '${MYSQL_USER}'@'%'IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql -u root
+	echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" | mysql -u root
 	
-	#DELETE FROM lines are for security, the first one deletes all users from the MYSQL
-	#instance except for the root
-	#removes all root users that can connect from hosts other than localhost, 127.0.0.1, or ::1.
-	#remove also all users where the user column is an empty string
+	echo "FLUSH PRIVILEGES;" | mysql -u root
 
-	#then we create a a new user, and specify its password, this user can connect from any machine
-	#on the network
-	#we grant all privileges, meaning the user will be able to have full access to the database
-	#and perform any action
-
-	#flushing the privileges allows to reload the MSQL server's grant tables and update the in-memory
-	#privileges with any changes made.
-	mysql -u root << EOF
-	
-	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-
-	DELETE FROM mysql.user WHERE user='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-	DELETE FROM mysql.user WHERE user='';
-
-	CREATE USER '${MYSQL_USER}'@'%'IDENTIFIED BY '${MYSQL_PASSWORD}';
-	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-	
-	FLUSH PRIVILEGES;
-
-EOF
-	killall mysqld 2> /dev/null
 fi
+
+killall mysqld 2> /dev/null
 
 
 exec "$@"
--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -19,7 +19,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --allow-root;
     echo "WordPress script: Downloaded..."
 	echo "WordPress plugin: creating users..."
-	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${$MYSQL_USER} --dbpass=${$MYSQL_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
 	echo "WordPress: set up!"
