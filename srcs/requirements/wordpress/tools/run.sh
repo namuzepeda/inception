cd99346a7ab1a9715a29710b4feae025f9c0b3ad
Nicolas MUNOZ <nmunoz@paul-f3Br2s2.clusters.42paris.fr>
2023-09-26 17:55:22 +0200

    Second fix for mariadb credentials

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -19,8 +19,8 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core download --allow-root;
     echo "WordPress script: Downloaded..."
 	echo "WordPress plugin: creating users..."
+	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${$MYSQL_USER} --dbpass=${$MYSQL_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${$MYSQL_USER} --dbpass=${$MYSQL_PASSWORD} --dbhost=172.18.0.1:3306 --path='/var/www/wordpress'
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
 	echo "WordPress: set up!"
 fi
