68c965bc1aedcd17542fc48dd47d3c4f6d21cc29
Nicolas MUNOZ <nmunoz@paul-f3Br2s2.clusters.42paris.fr>
2023-09-26 17:50:53 +0200

    Fixed mysql database credentials

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -20,7 +20,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     echo "WordPress script: Downloaded..."
 	echo "WordPress plugin: creating users..."
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	wp config create
+	wp config create --allow-root --dbname=${MYSQL_DATABASE} --dbuser=${$MYSQL_USER} --dbpass=${$MYSQL_PASSWORD} --dbhost=172.18.0.1:3306 --path='/var/www/wordpress'
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
 	echo "WordPress: set up!"
 fi
