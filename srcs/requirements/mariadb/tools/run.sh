a4834625a5935b385beefb7035a1cb3abd039ad2
Nicolas MUNOZ <nmunoz@paul-f3Br2s2.clusters.42paris.fr>
2023-10-02 20:44:37 +0200

    Debugging run.sh

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -5,11 +5,13 @@
 systemctl start mysqld
 
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
+
+cat db1.sql
 
 mysql < db1.sql
 
