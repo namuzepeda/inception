090a52f0365a9f19387833b9f7f2e73187aeba6c
Nicolas MUNOZ <nmunoz@paul-f3Br2s2.clusters.42paris.fr>
2023-10-02 20:29:58 +0200

    Trying new mariadb script

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
 
-set -eux
-
-DIRECTORY_DATABASE=/var/lib/mysql/wordpress
-
-echo "TEST-0"
-
-if [ ! -d "$DIRECTORY_DATABASE" ]; then
-
-	echo "TEST1"
-	
-	echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" | sudo mysql -u root
-
-	echo "TEST2"
-
-	echo $?
-
-	echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | sudo mysql -u root
-
-	echo $?
-
-	echo "CREATE USER '${MYSQL_USER}'@'%'IDENTIFIED BY '${MYSQL_PASSWORD}';" | sudo mysql -u root
-
-	echo $?
-
-	echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" | sudo mysql -u root
-
-	echo $?
-	
-	echo "FLUSH PRIVILEGES;" | sudo mysql -u root
-
-	echo $?
-
-	echo "FINISH"
-
-fi
-
-killall mysqld 2> /dev/null
 
 
-exec "$@"
+service mysql start 
+
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
