df10e0df4f3c71b7cfcb135f614b897d12152a8f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-05 14:09:18 +0200

    Changed wordpress script

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -6,20 +6,40 @@
 	mkdir -p /run/php/;
 	touch /run/php/php7.3-fpm.pid;
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-	cd /var/www/html
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOST/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+# Verificar si existe una instalación de WordPress en /var/www/html
+if [ -d "/var/www/html/wp-config.php" ]; then
+  echo "Ya existe una instalación de WordPress en /var/www/html."
+  exit 1
 fi
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+sudo mkdir -p /var/www/html
+
+cd /var/www/html
+wp core download --allow-root
+
+wp config create --allow-root \
+  --dbhost="$MYSQL_HOST" \
+  --dbname="$MYSQL_DATABASE" \
+  --dbuser="$MYSQL_USER" \
+  --dbpass="$MYSQL_PASSWORD"
+
+wp core install --allow-root \
+  --url="$WP_URL" \
+  --title="$WP_TITLE" \
+  --admin_user="$WP_ADMIN_LOGIN" \
+  --admin_password="$WP_ADMIN_PASSWORD" \
+  --admin_email="$WP_ADMIN_EMAIL"
+
+# Crear un usuario de prueba
+wp user create --allow-root \
+  "$WP_USER_LOGIN" \
+  "$WP_USER_EMAIL" \
+  --user_pass="$WP_USER_PASSWORD"
+
+echo "WordPress se ha instalado correctamente en /var/www/html."
 
 exec "$@"
