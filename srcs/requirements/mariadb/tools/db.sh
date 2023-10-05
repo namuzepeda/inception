00b7e54a027633200d2ff10e9481df2291c7792d
root <root@nmunoz.42.fr>
2023-10-05 14:02:20 +0200

    MariaDB Working, fixed with sudo

---
+++ srcs/requirements/mariadb/tools/db.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Start the MySQL service
+service mysql start
+
+# Wait for MySQL to start
+while ! mysqladmin ping -hlocalhost -u$MYSQL_USER -p$MYSQL_PASSWORD --silent; do
+    sleep 1
+done
+
+# Create the user and grant privileges
+sudo mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+# Create the database
+sudo mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+
+# Stop the MySQL service
+service mysql stop
+
+# Start the MySQL service in the background (as a daemon)
+exec mysqld
