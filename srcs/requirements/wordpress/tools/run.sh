0ae3baf0fa880cf7ff8f3f242fa67f13531f5f4f
BuildTools <unconfigured@null.spigotmc.org>
2023-10-05 14:19:00 +0200

    Changed from curl to wget

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -16,7 +16,7 @@ mkdir -p /var/www/html
 
 cd /var/www/html
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
